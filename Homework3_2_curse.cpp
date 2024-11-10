e742e88f94ec7b1eac03c9ec11b3b6b8646f9dab
lilia <lilia@DESKTOP-64J80RS>
2024-11-10 15:28:16 +0200

    Добавьте файлы проекта.

---
+++ Homework3_2_curse.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <cmath>
+
+int main()
+{
+    const char* myname="Liliya\n";
+    std::cout << myname;
+    float backyard=124.59;
+    std::cout << backyard<<std::endl;
+    long double stars=3e22;
+    std::cout << stars<<std::endl;
+    float averageprecipitation=57.5;
+    std::cout << averageprecipitation<<std::endl;
+    return 0;
+    
+}
